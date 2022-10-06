9df546bff04aa7622d81505910cc3354417a3960
DoanVuAnh <vuxanh051272001@gmail.com>
2022-10-06 15:00:15 +0700

    add file

--- Practice_UsingTheOperator/src/Index.java
+++ Practice_UsingTheOperator/src/Index.java
@@ -1,2 +1,13 @@
+import java.util.Scanner;
+
 public class Index {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Length: ");
+        int length = input.nextInt();
+        System.out.print("Enter Width: ");
+        int width = input.nextInt();
+        int area = length * width;
+        System.out.print("Area is: " + area);
+    }
 }
